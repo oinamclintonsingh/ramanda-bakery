abf4827468cbfd710abea9355b311695b79652d1
Oinam Clinton Singh <oinamclinton.co@gmail.com>
2024-10-07 22:48:40 +0530

    Ahanba Uplaod

---
+++ assets/js/scroll.js
@@ -0,0 +1,38 @@
+
+/* ================================================================ */
+/* Description gee Scroll css sida houre */
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for reduced motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // Add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // Add aria-hidden to it
+    // Add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+
+    // Add mouseenter and mouseleave event listeners to stop/restart scrolling
+    scroller.addEventListener("mouseenter", () => {
+      scroller.style.animationPlayState = 'paused'; // Stop animation
+    });
+
+    scroller.addEventListener("mouseleave", () => {
+      scroller.style.animationPlayState = 'running'; // Resume animation
+    });
+  });
+}
